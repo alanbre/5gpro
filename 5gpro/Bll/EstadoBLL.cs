b1e6abd26a06628d091e6a22e99d3c4ac447c410
Alan Soder <alanbr4@gmail.com>
2019-03-21 21:23:41 -0300

    Busca e carregamento da cidade na tela cadastrapessoa funcionando

---
+++ 5gpro/Bll/EstadoBLL.cs
@@ -0,0 +1,20 @@
+﻿using _5gpro.Daos;
+using _5gpro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5gpro.Bll
+{
+    class EstadoBLL
+    {
+        EstadoDAO EstadoDAO = new EstadoDAO();
+
+        public Estado BuscaEstadoByCod(string cod)
+        {
+            return EstadoDAO.BuscaEstadoByCod(cod);
+        }
+    }
+}
---
+++ 5gpro/Entities/Estado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5gpro.Entities
+{
+    public class Estado
+    {
+        public string CodEstado { get; set; }
+        public string Nome { get; set; }
+    }
+}
