af4b5fb496948441fd9e415e9da82e10624fe0fd
eduardo gomes <eduardo.r.g.d@gmail.com>
2019-04-18 20:12:18 -0300

    commi em casa quinta

--- 5gpro/Daos/ConexaoDAO.cs
+++ 5gpro/Daos/ConexaoDAO.cs
@@ -5,8 +5,15 @@ namespace _5gpro.Daos
 {
     public class ConexaoDAO
     {
-        public static string Conecta = "DATABASE=5gprodatabase; SERVER=localhost; UID=5gprouser; PWD=5gproedualan";
+        //Localhost
+        //public static string Conecta = "DATABASE=5gprodatabase; SERVER=localhost; UID=5gprouser; PWD=5gproedualan";
+
+        //DALVA
         //public static string Conecta = "DATABASE=5gprodatabase; SERVER=192.168.2.111; UID=5gprouser; PWD=5gproedualan; pooling = true";
+
+        //Casa Eduardo Note
+        public static string Conecta = "DATABASE=5gprodatabase; SERVER=192.168.0.103; UID=5gprouser; PWD=5gproedualan; pooling = true";
+
         public MySqlConnection Conexao;
         public MySqlTransaction tr = null;
         public MySqlCommand Comando = null;
